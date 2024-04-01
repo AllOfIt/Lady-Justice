f3fefa08b941302149ba5baec5c5f925c1c114b4
Chris Tullier <christopher.tullier98@gmail.com>
2024-04-01 00:54:06 -0400

    trying to get members

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
 import discord
+from discord.ext.commands import MissingPermissions, has_permissions
 
 from config import TOKEN
-
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f"Logged on as {self.user}!")
-
-    async def on_message(self, message):
-        print(f"Message from {message.author}: {message.content}")
-        if message.content.startswith("ban"):
-            user = message.content.split()[1]
-            print(f"Ban user: {user}")
-            # ban function
-
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message: discord.Message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+
+    if message.content.startswith("ban"):
+        user_id = message.content.split()[1]
+
+        # todo right now this only prints out the bot's member information
+        # why can't it see the other members?
+        print(message.guild.members)
+        print(f"ban: {user_id}")
+
+
+@client.event
+async def ban(
+    user, *, reason=None, delete_message_days=..., delete_message_seconds=...
+):
+    print(f"Banning {user}")
+
+
 client.run(TOKEN)
