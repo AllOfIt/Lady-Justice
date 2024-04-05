37a8c0440a636779fc8c323f2a0395288ed090e5
AllOfIt <lukester9010@gmail.com>
2024-04-04 23:51:40 -0500

    User wrapper class skeleton

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import datetime
 from discord.ext.commands import MissingPermissions, has_permissions
 
 from config import TOKEN
@@ -8,6 +9,48 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+# wrapper for discord.py user class to better serve our purposes
+class User:
+    def __init__(self,id:int,client:discord.Client):
+        self.userObject:discord.User = client.get_user(id)
+        self.name = ""
+        self.allegiance:User = None
+        self.secretAllegiance:User = None
+        self.bannedDate:datetime.datetime = None
+        self.permaBanned:bool = False
+
+        self.updateDatabase(True)
+
+    def updateDatabase(self,newEntry:bool = False):
+        if newEntry:
+            pass
+        else:
+            pass
+
+    def addRole(self,role):
+        pass
+    
+    def removeRole(self,role):
+        pass
+
+    #might need a system for searching for the right user class, maybe a dict
+    def setAllegiance(self,target):
+        self.allegiance = target
+        self.updateDatabase()
+    
+    def setSecretAllegiance(self,target):
+        self.secretAllegiance = target
+        self.updateDatabase()
+
+    def ban(self):
+        self.bannedDate = datetime.datetime.now()
+        #this probably won't work I just Guessed
+        self.userObject.guild.ban(self.userObject)
+
+    def permaBan(self):
+        self.bannedDate = None
+        self.permaBanned = True
+        self.userObject.guild.ban(self.userObject)
 
 @client.event
 async def on_ready():
