f5e586e23c93e701b317b592e6a1581b0025db6a
Chris Tullier <christopher.tullier98@gmail.com>
2024-04-12 22:32:25 -0400

    formatting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import discord
 import datetime
+
+import discord
 from discord.ext.commands import MissingPermissions, has_permissions
 
 from config import TOKEN
@@ -8,12 +9,13 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-ANNOUNCEMENTS = 895310090599559188 # How it works
+ANNOUNCEMENTS = 895310090599559188  # How it works
 ROUND_TABLE = 895309794561368064
 GENERAL = 895296887408701471
 RATS = 895310870148706344
 
-#superclass for anything you can lend your support to
+
+# superclass for anything you can lend your support to
 class Supportable:
     def __init_subclass__(self):
         self.name = ""
@@ -22,35 +24,36 @@ class Supportable:
 
     def __str__(self):
         return self.name
-    
-    #supporter should be a User instance but ":User" causes an error because its not difined yet :(
-    def support(self,supporter):
+
+    # supporter should be a User instance but ":User" causes an error because its not difined yet :(
+    def support(self, supporter):
         if supporter in self.supporters:
             return f"You already support {self}"
         self.supporters.append(supporter)
         return f"You have pledged your support to {self}"
-    
-    #same issue with ":User"
-    def removeSupport(self,supporter):
+
+    # same issue with ":User"
+    def removeSupport(self, supporter):
         if supporter in self.supporters:
             self.supporters.remove(supporter)
             return f"You have withdrawn your support from {self}"
         return f"You don't support {self}]"
-    
+
+
 # wrapper for discord.py user class to better serve our purposes
 # the global user list (allUsers) should be passed as the userList arguement
 class User(Supportable):
-    def __init__(self,id:int,client:discord.Client,userList:dict):
-        #adds the new user to the global user list
+    def __init__(self, id: int, client: discord.Client, userList: dict):
+        # adds the new user to the global user list
         userList[id] = self
         self.id = id
-        self.userObject:discord.User = client.get_user(id)
-        self.userList:dict = userList
+        self.userObject: discord.User = client.get_user(id)
+        self.userList: dict = userList
         self.name = ""
-        self.allegiance:User = None
-        self.secretAllegiance:User = None
-        self.bannedDate:datetime.datetime = None
-        self.permaBanned:bool = False
+        self.allegiance: User = None
+        self.secretAllegiance: User = None
+        self.bannedDate: datetime.datetime = None
+        self.permaBanned: bool = False
         self.supporters = []
 
         self.updateDatabase(True)
@@ -58,67 +61,74 @@ class User(Supportable):
     def __str__(self):
         return self.userObject.display_name()
 
-    def updateDatabase(self,newEntry:bool = False):
+    def updateDatabase(self, newEntry: bool = False):
         if newEntry:
             pass
         else:
             pass
 
-    def addRole(self,role):
+    def addRole(self, role):
         pass
-    
-    def removeRole(self,role):
+
+    def removeRole(self, role):
         pass
 
     # todo: this needs fixing
     # target for both of these can be any supportable
-    def setAllegiance(self,target):
-        
+    def setAllegiance(self, target):
+
         self.allegiance = target
 
         self.updateDatabase()
-    
-    def setSecretAllegiance(self,target):
+
+    def setSecretAllegiance(self, target):
         pass
 
     def ban(self):
         self.bannedDate = datetime.datetime.now()
-        #this probably won't work I just Guessed
+        # this probably won't work I just Guessed
         self.userObject.guild.ban(self.userObject)
 
     def permaBan(self):
         self.permaBanned = True
         self.ban()
 
-    async def message(self,message):
+    async def message(self, message):
         await self.userObject.dm_channel.send(message)
+
 
 # government superclass
 class Government(Supportable):
-    def __init__(self,leaders):
+    def __init__(self, leaders):
         self.leaders = leaders
         self.openVotes = []
         self.openMotions = []
 
     # participants are the people who can vote, channel is where the vote takes place, action is function to run if the vote passes, duration is how long the vote will last
     # Example: currentGovernment.startVote(currentGovernment.leaders,ROUND_TABLE,Joeuser.ban())
-    def startVote(self,participants,channel,action,duration):
+    def startVote(self, participants, channel, action, duration):
         pass
+
 
 class Anarchy(Government):
     pass
 
+
 class Dictatorship(Government):
     pass
+
 
 class Monarchy(Government):
     pass
 
+
 class Oligarchy(Government):
     pass
 
+
 class Democracy(Government):
     pass
+
 
 class Republic(Government):
     pass
@@ -126,7 +136,7 @@ class Republic(Government):
 
 # Motion Superclass that lets player define a power move and set their allegiance to it before it takes effect
 class Motion(Supportable):
-    def __init__(self,name:str,initiator:User, government:Government,action):
+    def __init__(self, name: str, initiator: User, government: Government, action):
         self.name = name
         self.initiator = initiator
         self.government = government
@@ -148,15 +158,17 @@ class Motion(Supportable):
         self.delete()
         return self.exitMessage
 
+
 # also a superclass, this type of motion seeks to start a new government
 class TakeOver(Motion):
-    def __init__(self,newGovernment:Government):
+    def __init__(self, newGovernment: Government):
         super().__init__(self)
         self.newGovernment = newGovernment
 
     def passMotion(self):
         global currentGovernment
         currentGovernment = self.newGovernment
+
 
 class Vote:
     def __init__(self):
@@ -165,6 +177,7 @@ class Vote:
 
 allUsers = {}
 currentGovernment = Anarchy([])
+
 
 @client.event
 async def on_ready():
